951c6c7551a4a08c61d26fc244fc0cd9a0c32c0f
Minji Kim <59950900+minjikim23@users.noreply.github.com>
2023-04-01 12:58:21 +0200

    integrate monitoring
    Signed-off-by: Minji Kim <minji.kim@3m5.de>

---
+++ app/controllers/MonitoringController.scala
@@ -0,0 +1,46 @@
+/*
+ * This file is part of the "issue board" modicio case study software.
+ * Copyright (C) 2022 Karl Kegel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * */
+
+package controllers
+
+import env.{RegistryProvider, VirtualFileSystem, VirtualRegistryExtension}
+import modules.instances.service.InstanceService
+import modules.model.service.ModelService
+import play.api.Logging
+import play.api.i18n.I18nSupport
+import play.api.mvc._
+
+import javax.inject.{Inject, Singleton}
+
+@Singleton
+class ModelController @Inject()(cc: ControllerComponents) extends
+  AbstractController(cc) with I18nSupport with Logging {
+  
+  def index: Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
+    for {
+      registry <- RegistryProvider.getRegistry
+      references <- registry.getReferences
+    } yield {
+      
+      val allClasses = registry.produceJson()
+      
+      Ok(views.html.pages.monitoring(allClasses))
+    }
+  }
+
+}
